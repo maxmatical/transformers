f9f9fc5040649e0ea5d92820191d3a3477946802
Max Tian <max.tian.94@outlook.com>
2019-12-12 10:33:16 -0500

    separated files for albert versions

---
+++ run_squad_albert_v2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --mem=12G
+#SBATCH -c 2
+#SBATCH --gres=gpu:1
+#SBATCH -p nlp
+python3 ./examples/run_squad_max.py \
+    --model_type albert \
+    --model_name_or_path albert-large-v2 \
+    --do_train \
+    --do_eval \
+    --do_lower_case \
+    --version_2_with_negative \
+    --train_file /scratch/gobi1/mtian/SQUAD/train-v2.0.json \
+    --predict_file /scratch/gobi1/mtian/SQUAD/dev-v2.0.json \
+    --learning_rate 3e-5 \
+    --weight_decay 0 \
+    --beta1 0.9 \
+    --beta2 0.98 \
+    --adam_epsilon 1e-8 \
+    --lr_scheduler 'cosine' \
+    --max_steps 8144 \
+    --save_steps 500 \
+    --warmup_steps 814 \
+    --max_seq_length 512 \
+    --doc_stride 128 \
+    --output_dir ./output/models/albert_squad_max_beta2_98_v2_lowercase/ \
+    --overwrite_output_dir \
+    --gradient_accumulation_steps 16 \
+    --per_gpu_eval_batch_size=3   \
+    --per_gpu_train_batch_size=3   \
