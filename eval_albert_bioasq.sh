5c7ecd6b71e37167deb04c60ce2a3f0596da00f6
Max Tian <max.tian.94@outlook.com>
2020-01-23 15:33:41 -0500

    updates

--- eval_albert_bioasq.sh
+++ eval_albert_bioasq.sh
@@ -8,15 +8,13 @@ python3 ./examples/run_squad_max.py \
    --model_name_or_path ./output/models/bioasq_albert_v2_lre3-5/ \
    --do_eval \
    --do_lower_case \
-   --train_file dir/BioASQ-train-factoid-4b.json \
-   --predict_file dir/BioASQ-test-factoid-4b-1.json \
+   --predict_file /scratch/gobi1/mtian/BioASQ/BioASQ-test-factoid-4b-1.json \
    --learning_rate 3e-5 \
    --num_train_epochs 5 \
    --save_steps 30000 \
    --max_seq_length 384 \
    --doc_stride 128 \
-   --output_dir ./output/models/albert_bioasq/4b1/ \
+   --output_dir ./output/bioasq/ \
    --overwrite_output_dir \
-   --gradient_accumulation_steps 6\
-   --per_gpu_eval_batch_size=2  \
-   --per_gpu_train_batch_size=2 
+   --gradient_accumulation_steps 6 \
+   --per_gpu_eval_batch_size=2
