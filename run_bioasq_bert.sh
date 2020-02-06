c448a6943d2bdab75f2916be6ec8d8e2f2a5a97f
Max Tian <max.tian.94@outlook.com>
2020-02-06 14:08:31 -0500

    feb6 1 epoch

--- run_bioasq_bert.sh
+++ run_bioasq_bert.sh
@@ -16,10 +16,10 @@ python3 ./examples/run_squad_max.py \
     --beta2 0.98 \
     --adam_epsilon 1e-8 \
     --lr_scheduler 'cosine' \
-    --num_train_epochs 2 \
+    --num_train_epochs 1 \
     --max_seq_length 384 \
     --doc_stride 128 \
-    --output_dir /scratch/gobi1/mtian/models/bioasq_bert_lre5-6_sq/ \
+    --output_dir /scratch/gobi1/mtian/models/bioasq_bert_1epoch_lre5-6_sq/ \
     --overwrite_output_dir \
     --gradient_accumulation_steps 16 \
     --per_gpu_eval_batch_size=2   \
