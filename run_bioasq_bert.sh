8244db9c07f403960e988a6271d49aa429db77a0
mtian <max.tian.94@outlook.com>
2020-03-17 10:51:26 -0400

    mar16

--- run_bioasq_bert.sh
+++ run_bioasq_bert.sh
@@ -8,7 +8,7 @@ python3 ./examples/run_squad_max.py \
     --model_name_or_path /scratch/gobi1/mtian/models/bert_squadv1_max_beta2_98_lr_3e-5/ \
     --do_train \
     --do_lower_case \
-    --train_file /scratch/gobi1/mtian/BioASQ/BioASQ-train-factoid-4b.json  \
+    --train_file /scratch/gobi1/mtian/BioASQ/BioASQ-train-factoid-6b.json  \
     --predict_file /scratch/gobi1/mtian/BioASQ/BioASQ-test-factoid-4b-1.json \
     --learning_rate 5e-6 \
     --weight_decay 0 \
@@ -20,7 +20,7 @@ python3 ./examples/run_squad_max.py \
     --save_steps 1000 \
     --max_seq_length 384 \
     --doc_stride 128 \
-    --output_dir /scratch/gobi1/mtian/models/bioasq_bert_20epoch_lre5-6_sq/ \
+    --output_dir /scratch/gobi1/mtian/models/bioasq_bert_20epoch_lre5-6_sq_6b/ \
     --overwrite_output_dir \
     --gradient_accumulation_steps 16 \
     --per_gpu_eval_batch_size=2   \
